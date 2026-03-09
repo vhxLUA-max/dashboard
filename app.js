9901988331cf3df62f3e9f47f22c27cb766d1d94
vhxLUA-max <nageraadrian51@gmail.com>
2026-03-09 16:27:50 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ExecutionDashboard from './components/ExecutionDashboard';
+import './App.css';
+
+function App() {
+  return (
+    <ExecutionDashboard />
+  );
+}
+
+export default App;
